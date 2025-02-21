703a1e52232657dc3fa29ed240071ef7e5ec47bb
JefWinKamaz <jef239@mail.ru>
2025-02-21 18:14:22 +0200

    LED "Internet" on ELT0x33

--- Install/rtkbase_check_internet.sh
+++ Install/rtkbase_check_internet.sh
@@ -4,21 +4,55 @@
 HAVE_PI4=`cat /proc/cpuinfo | grep Model | grep "Pi 4"`
 HAVE_TYPEC=`lsusb | grep "Bus 003" | grep -v "root hub"`
 HAVE_DEB12=`lsb_release -c | grep bookworm`
-if [[ "${HAVE_PI4}" == "" ]] || [[ "${HAVE_TYPEC}" == "" ]] || [[ "${HAVE_DEB12}" == "" ]]; then
-   echo No PI4 or no Debian 12 or haven\'t USB device in type-C
-   echo HAVE_PI4=${HAVE_PI4} HAVE_TYPEC=${HAVE_TYPEC} HAVE_DEB12=${HAVE_DEB12}
+HAVE_ELT0x33=`find -P /dev/serial/by-id -name "*ELT0x33*"`
+if [[ "${HAVE_ELT0x33}" == "" ]]; then
+   if [[ "${HAVE_PI4}" != "" ]] && [[ "${HAVE_TYPEC}" != "" ]] && [[ "${HAVE_DEB12}" != "" ]]; then
+      USE_FTDI=N
+      GPIO=16
+   fi
+else
+   for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name product); do
+       product=`cat ${sysdevpath}`
+       if [[ "${product}" == "ELT0x33" ]]; then
+          syspath="${sysdevpath%/product}"
+          #echo syspath=${syspath}
+          #echo find ${syspath}/*/gpiochip* -name  "gpiochip*"
+          gpiochip=`find ${syspath}/*/gpiochip* -name  "gpiochip*"`
+          #echo gpiochip=${gpiochip}
+          if [[ "${gpiochip}" != "" ]]; then
+             CHIP=`echo ${gpiochip} | sed s/^.*gpiochip//`
+             #echo CHIP=${CHIP}
+             USE_FTDI=Y
+             GPIO=2
+         fi
+       fi
+   done
+fi
+
+if [[ "${USE_FTDI}" == "" ]]; then
+   echo No PI4 or no Debian 12 or haven\'t USB device in type-C or no ELT0x33
+   echo HAVE_PI4=${HAVE_PI4} HAVE_TYPEC=${HAVE_TYPEC} HAVE_DEB12=${HAVE_DEB12} HAVE_ELT0x33=${HAVE_ELT0x33}
    systemctl stop rtkbase_check_internet.service
    exit 0
 fi
 
+#echo USE_FTDI="${USE_FTDI}" GPIO="${GPIO}" CHIP="${CHIP}"
+
+set_gpio(){
+if [[ "${USE_FTDI}" = "Y" ]]; then
+   gpioset gpiochip${CHIP} ${GPIO}=${1}
+else
+   pinctrl set ${GPIO} ${2} ${3}
+fi
+}
+
 FLAG=/usr/local/rtkbase/NetworkChange.flg
-GPIO=16
-pinctrl set ${GPIO} op dl
 state=DOWN
+set_gpio 0 op dl
 
 while : ; do
-   #echo ping -4 -c 1 -W 1 -q 8.8.8.8 \>/dev/null
-   ping -4 -c 1 -W 1 -q 8.8.8.8 >/dev/null
+   #echo ping -4 -c 1 -W 1 -q 8.8.8.8 \>/dev/null 2\>\&1
+   ping -4 -c 1 -W 1 -q 8.8.8.8 >/dev/null 2>&1
    lastcode=$?
    if [[ ${lastcode} == 0 ]]; then
       newstate=UP
@@ -29,10 +63,10 @@ while : ; do
    if [[ "${newstate}" != "${state}" ]]; then
       if [ "${newstate}" == "UP" ]; then
          echo Internet UP
-         pinctrl set ${GPIO} dh
+         set_gpio 1 dh
       else
          echo Internet DOWN
-         pinctrl set ${GPIO} dl
+         set_gpio 0 dl
       fi
       state=${newstate}
    fi
