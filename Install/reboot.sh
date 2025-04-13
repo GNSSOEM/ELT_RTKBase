3f6c80f487f02441889005b0999c1e69d0f9ed8d
JefWinKamaz <jef239@mail.ru>
2025-04-13 19:03:18 +0300

    MOSAIC: Internet and NTRIP LED off before reboot

--- Install/reboot.sh
+++ Install/reboot.sh
@@ -4,9 +4,21 @@ HAVE_MOSAIC=`find -P /dev/serial/by-id -name "*Septentrio*" 2>/dev/null`
 #echo HAVE_ELT0x33=${HAVE_ELT0x33} HAVE_MOSAIC=${HAVE_MOSAIC}
 if [[ "${HAVE_ELT0x33}" == "" ]] && [[ "${HAVE_MOSAIC}" != "" ]]; then
    if [[ -c /dev/ttyGNSS_CTRL ]]; then
-      device=`readlink -f /dev/ttyGNSS_CTRL`
+      MOSAIC=`readlink -f /dev/ttyGNSS_CTRL`
       BASEDIR="$(dirname $(dirname "$0"))"
-      #echo "${BASEDIR}"/NmeaConf "${device}" "setDataInOut,USB1,CMD,none" QUIET
-      "${BASEDIR}"/NmeaConf "${device}" "setDataInOut,USB1,CMD,none" QUIET
+      for i in `seq 1 5`; do
+          #echo "${BASEDIR}"/NmeaConf "${MOSAIC}" "setDataInOut,USB1,CMD,none" QUIET
+          "${BASEDIR}"/NmeaConf "${MOSAIC}" "setDataInOut,USB1,CMD,none" QUIET
+          if [[ "$?" == "0" ]]; then
+             break
+         fi
+      done
+      for i in `seq 1 5`; do
+          #echo "${BASEDIR}"/NmeaConf "${MOSAIC}" "setGPIOFunctionality,all,Output,none,LevelLow" QUIET
+          "${BASEDIR}"/NmeaConf "${MOSAIC}" "setGPIOFunctionality,all,Output,none,LevelLow" QUIET
+          if [[ "$?" == "0" ]]; then
+             break
+         fi
+      done
    fi
 fi
