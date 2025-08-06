7b5e262a31108b7813ba8d0b5d5f6d411ce01ca7
JefWinKamaz <jef239@mail.ru>
2025-08-06 18:09:12 +0300

    Update show_wifi_list.sh

--- Tools/show_wifi_list.sh
+++ Tools/show_wifi_list.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 nmcli device wifi rescan
-nmcli device wifi list
+nmcli -f SIGNAL,SSID,FREQ,CHAN,ACTIVE,IN-USE  device wifi lis
+nmcli -f device,type,autoconnect,active,state connection show
+lsusb
 
 if [[ ${1} == "-s" ]]; then
    exit 254
