c254bf240c01fab24c16db619cad342ba5ffc493
JefWinKamaz <jef239@mail.ru>
2025-08-19 04:51:06 +0300

    NTRIP v1 for Onocoy using ntripserver

--- Install/install_script.sh
+++ Install/install_script.sh
@@ -1486,13 +1486,13 @@ configure_settings(){
       #echo if ! grep -q \"^\[ntrip_D\]=\" \"${SETTINGS_NOW}\"\; then
       if ! grep -q "^\[ntrip_D\]" "${SETTINGS_NOW}"; then
          echo insert [ntrip_D] into ${SETTINGS_NOW}
-         ${sed} "/^ntrip_b_receiver_options=/a \ \n[ntrip_D]\n\n# NTRIP D caster options\n\n#ntrip D caster url\nsvr_addr_d=\'caster\.emlid\.com\'\n#ntrip D caster port\nsvr_port_d=\'2101\'\n#ntrip D caster mode\nsvr_mode_d=\'0\'\n#ntrip D caster login\nsvr_login_d=\'\'\n#ntrip D caster password\nsvr_pwd_d=\'\'\n#Mount name\nmnt_name_d=\'Your_mount_name\'\nrtcm_msg_d=\'${rtcm_msg}\'\n#Receiver dependent options\nntrip_d_receiver_options=\'\'" "${SETTINGS_NOW}"
+         ${sed} "/^ntrip_b_receiver_options=/a \ \n[ntrip_D]\n\n# NTRIP D caster options\n\n#ntrip D caster url\nsvr_addr_d=\'caster\.emlid\.com\'\n#ntrip D caster port\nsvr_port_d=\'2101\'\n#ntrip D caster mode\nsvr_mode_d=\'0\'\n#ntrip D caster login\nsvr_login_d=\'\'\n#ntrip D caster password\nsvr_pwd_d=\'\'\n#Mount name\nmnt_name_d=\'\'\nrtcm_msg_d=\'${rtcm_msg}\'\n#Receiver dependent options\nntrip_d_receiver_options=\'\'" "${SETTINGS_NOW}"
       fi
 
       #echo if ! grep -q \"^\[ntrip_C\]=\" \"${SETTINGS_NOW}\"\; then
       if ! grep -q "^\[ntrip_C\]" "${SETTINGS_NOW}"; then
          echo insert [ntrip_C] into ${SETTINGS_NOW}
-         ${sed} "/^ntrip_b_receiver_options=/a \ \n[ntrip_C]\n\n# NTRIP C caster options\n\n#ntrip C caster url\nsvr_addr_c=\'rtk2go\.com\'\n#ntrip C caster port\nsvr_port_c=\'2101\'\n#ntrip C caster mode\nsvr_mode_c=\'2\'\n#ntrip C caster login\nsvr_login_c=\'\'\n#ntrip C caster password\nsvr_pwd_c=\'\'\n#Mount name\nmnt_name_c=\'Your_mount_name\'\nrtcm_msg_c=\'${rtcm_msg}\'\n#Receiver dependent options\nntrip_c_receiver_options=\'\'" "${SETTINGS_NOW}"
+         ${sed} "/^ntrip_b_receiver_options=/a \ \n[ntrip_C]\n\n# NTRIP C caster options\n\n#ntrip C caster url\nsvr_addr_c=\'rtk2go\.com\'\n#ntrip C caster port\nsvr_port_c=\'2101\'\n#ntrip C caster mode\nsvr_mode_c=\'2\'\n#ntrip C caster login\nsvr_login_c=\'\'\n#ntrip C caster password\nsvr_pwd_c=\'\'\n#Mount name\nmnt_name_c=\'\'\nrtcm_msg_c=\'${rtcm_msg}\'\n#Receiver dependent options\nntrip_c_receiver_options=\'\'" "${SETTINGS_NOW}"
       fi
 
       #echo if ! grep -q "^mobile_modem_web_ip=" "${SETTINGS_NOW}"; then
