9dbf486b4d6c79d9f121732771d20b6ce7ac5b18
JefWinKamaz <jef239@mail.ru>
2025-10-19 22:54:23 +0300

    WPS - Remove connections of the same name before adding

--- Install/PBC.sh
+++ Install/PBC.sh
@@ -140,6 +140,8 @@ $white▐█$purpple   WPA Key:$yellow $wpakey$nocolour"
                 echo -e "$white▐█$purpple   Restarting Newtork Manager$nocolour"
                 systemctl restart NetworkManager.service
                 echo -e "$white▐█$purpple   Exporting profile for$yellow $essid$purpple to Network Manager$nocolour"
+                already=$(sudo nmcli --get-values NAME connection show | grep "$essid")
+                [[ "$already" != "" ]] && nmcli connection delete id "$essid"
                 nmcli con add con-name "$essid" ifname "*" type wifi ssid "$essid"
                 nmcli con modify "$essid" wifi-sec.key-mgmt wpa-psk 
                 nmcli con modify "$essid" wifi-sec.psk "$wpakey" 
