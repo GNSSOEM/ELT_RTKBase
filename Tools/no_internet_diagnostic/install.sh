0c647a5f4705c00d774e2036cbdda52494133ddb
JefWinKamaz <jef239@mail.ru>
2025-12-05 03:25:38 +0200

    Diagnosing a lack of internet connection by updating from an SD card

---
+++ Tools/no_internet_diagnostic/install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+LOG=/boot/firmware/log
+if [[ ! -d ${LOG} ]]; then
+   mkdir ${LOG}
+fi
+
+SYSLOG=${LOG}/systemlog.txt
+nmcli device wifi rescan 
+nmcli -f SIGNAL,SSID,FREQ,CHAN,ACTIVE,IN-USE  device wifi list >${SYSLOG}
+nmcli -f device,type,autoconnect,active,state connection show >>${SYSLOG}
+lsusb >>${SYSLOG}
+ls -la /dev/serial/by-id/ >>${SYSLOG}
+
+serviceList="NetworkManager \
+             RtkbaseSystemConfigure \
+             rtkbase_web \
+             str2str_tcp \
+             rtkbase_check_internet \
+             rtkbase_check_satelites \
+             rtkbase_septentrio_NAT \
+             rtkbase_DHCP"
+
+for service_name in ${serviceList}; do
+    journalctl -u "${service_name}.service" --reverse >"${LOG}/${service_name}.txt" 2>&1
+done
